cba166d39910fbbc5f0296bb9077e3f94122c93b
digiot <digiot.abc@gmail.com>
2024-04-03 11:03:15 +0900

    期間を追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row v-for="item in notices" :key="item.year">
       <v-col cols="12">
         <v-card tile hover>
-          <v-card-title>2024</v-card-title>
-          <v-divider/>
-          <v-card-text>
+          <v-card-title>{{ item.year }} 年度</v-card-title>
+          <v-card-subtitle>
+            期間: {{ item.from }} - {{ item.to }}
+          </v-card-subtitle>
+          <v-divider class="mt-3"/>
+          <v-card-text v-if="item.balance_sheet !== ''">
+            {{ item.balance_sheet }}
+          </v-card-text>
+          <v-card-text v-else>
             未掲載
           </v-card-text>
         </v-card>
@@ -15,4 +21,15 @@
 </template>
 
 <script lang="ts" setup>
+
+import {ref} from "vue";
+
+const notices = ref([
+  {
+    "year": "2024",
+    "from": "2024-04-01",
+    "to": "2025-03-31",
+    "balance_sheet": ""
+  }
+])
 </script>
